a11d59fd97c3d4ae7be3a9e22f1faded87891673
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-18 00:02:03 +0900

    ES6 Template Literals in JSX

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const name = 'Siam';
+const firstname = 'Siam';
+const lastname = 'Ahmed';
 
-// Javascript Expression
-// Expression is nothing but a combination of operand and operators
-// In curly bracks we only can use expression not statements
+// Using Expression 
+/*
 ReactDOM.render(
   <>
-    <h1>My name is { name }</h1>
-    <p>Addition: { 2 + 3 }</p>
-    <p>Random number: { Math.random() }</p>
+    <h1>My name is { firstname } { lastname }</h1>
+  </>,
+  document.getElementById('root')
+);
+*/
+
+// Using concatination
+/*
+ReactDOM.render(
+  <>
+    <h1>My name is { firstname + " " + lastname }</h1>
+  </>,
+  document.getElementById('root')
+);
+*/
+
+// Template Literals using backtick
+ReactDOM.render(
+  <>
+    <h1>My { ` name is ${firstname} ${lastname}` }</h1>
   </>,
   document.getElementById('root')
 );
