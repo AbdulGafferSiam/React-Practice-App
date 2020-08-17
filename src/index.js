7197464c7406b20d0c035274b7ba969794aa76f0
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-17 22:03:33 +0900

    Render Multiple Elements inside ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -12,10 +12,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // ReactDOM.render('what to show', 'where to show', 'callback func');
+// render method accept only one jsx element
+// To use multiple element, then need to wrap in one parent element
 ReactDOM.render(
-        <h1>Hello World!</h1>,
-        document.getElementById('root')
-      );
+  <dev>
+    <h1>Hello World!</h1>
+    <p>This is the extended element.</p>
+  </dev>,
+  document.getElementById('root')
+);
+
+// In react v16 it's possible for render() to return an array of elements
+/*
+ReactDOM.render(
+  [
+    <h1>Hello World!</h1>,
+    <p>This is the extended element.</p>,
+    <p>This is the another extended element.</p>
+  ],
+  document.getElementById('root')
+);
+*/
 
 // Ancient js representation of ReactDOM
 // Inside the jsx
