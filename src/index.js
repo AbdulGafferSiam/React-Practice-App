de8337a530d2d1d2b9d0fafb780ede578fc7b5da
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-17 23:50:56 +0900

    JavaScript Expressions in JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const name = 'Siam';
+
+// Javascript Expression
+// Expression is nothing but a combination of operand and operators
+// In curly bracks we only can use expression not statements
 ReactDOM.render(
   <>
-    <h1>Netflix Series</h1>
-    <p>List of 5 netflix series:</p>
-    <ol>
-      <li>Breaking Bad</li>
-      <li>Dark</li>
-      <li>Narcos</li>
-      <li>Secret Games</li>
-      <li>Money Heist</li>
-    </ol>
+    <h1>My name is { name }</h1>
+    <p>Addition: { 2 + 3 }</p>
+    <p>Random number: { Math.random() }</p>
   </>,
   document.getElementById('root')
 );
