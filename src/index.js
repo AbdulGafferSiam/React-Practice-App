20a003b33601a9545ac29ed5dbc7a81f7b3a0106
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-18 15:57:27 +0900

    Internal CSS & Inline CSS Styling

--- src/index.js
+++ src/index.js
@@ -8,9 +8,20 @@ const imgURL2 = "https://picsum.photos/536/355";
 const imgURL3 = "https://picsum.photos/536/356";
 const link = "http://www.siamsbucket.com/";
 
+// add css by creating as an object
+const cssHeader = {
+    color: '#fa9191',
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    fontWeight: 'bold',
+    textShadow: '0px 2px 4x #5e131f',
+    margin: '30px 0px',
+    fontFamily: '"Josefin Sans", sans-serif'
+};
+
 ReactDOM.render(
   <>
-    <h1 className="header">My name is { name } </h1>
+    <h1 className="header" style={ cssHeader }>My name is { name } </h1>
     <div className="img_div">
       <img src={imgURL1} alt="random images" />
       <img src={imgURL2} alt="random images" />
