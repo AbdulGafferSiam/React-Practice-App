766e20ea988f941a56e88556838ecb42265b4255
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-18 13:42:58 +0900

    JSX Attributes

--- src/index.js
+++ src/index.js
@@ -2,54 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const name = "Siam";
-const now = new Date();
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const imgURL1 = "https://picsum.photos/536/354";
+const imgURL2 = "https://picsum.photos/536/355";
+const imgURL3 = "https://picsum.photos/536/356";
+const link = "http://www.siamsbucket.com/";
 
-// toLocaleDateString(locale, options)
+// JSX Attributes
+// Use camel case of global attributes
 ReactDOM.render(
   <>
-    <h1>Hello, My name is { name }</h1>
-    <p>Todays Date: { now.toLocaleDateString() }</p>
-    <p>Todays Date: { now.toLocaleDateString("ja-JP", options) }</p>
-    <p>Todays Time: { now.toLocaleTimeString() }</p>
+    <h1 contentEditable="true">My name is { name } </h1>
+    <img src={imgURL1} alt="random images" />
+    <img src={imgURL2} alt="random images" />
+    <a href={link} target="_blank">
+      <img src={imgURL3} alt="random images" />
+    </a>
   </>,
   document.getElementById('root')
 );
-
-/*
-Options key examples:
-day:
-The representation of the day.
-Possible values are "numeric", "2-digit".
-weekday:
-The representation of the weekday.
-Possible values are "narrow", "short", "long".
-year:
-The representation of the year.
-Possible values are "numeric", "2-digit".
-month:
-The representation of the month.
-Possible values are "numeric", "2-digit", "narrow", "short", "long".
-hour:
-The representation of the hour.
-Possible values are "numeric", "2-digit".
-minute: The representation of the minute.
-Possible values are "numeric", "2-digit".
-second:
-The representation of the second.
-Possible values are "numeric", 2-digit".
-Note: If you would only like to configure the content options, but still use the current locale, 
-passing null for the first parameter will cause an error. Use undefined instead.
-
-Local key examples:
-For different languages:
-"en-US": For English
-"hi-IN": For Hindi
-"ja-JP": For Japanese
-You can use more language options.
-
-You can also use the toLocaleString() method for the same purpose. 
-The only difference is this function provides the time when you don't pass any options.
-Example:
-9/17/2016, 1:21:34 PM
-*/
