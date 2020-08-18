652add15e14194f572fe8608f2fbd4479b001d54
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-18 23:34:19 +0900

    Practice #4: Rewrite our Greeting Project into Components

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+    // new Date (year, month, day, hours, minutes, seconds, millis)
+    let currDate = new Date();
+    let hour = currDate.getHours();
+
+    let greeting = '';
+    let gtStyle = {};
+
+    if(hour < 12) {
+    greeting = "Good Morning";
+    gtStyle.color = "green";
+    } 
+    else if(12 <=hour && hour <= 19) {
+    greeting = "Good Afternoon";
+    gtStyle.color = "orange";
+    }
+    else{
+    greeting = "Good Night";
+    gtStyle.color = "black";
+    }
+    return (
+        <>
+            <div className="main-div">
+                <h1>Hello Sir, <span style={ gtStyle }> { greeting } </span></h1>
+            </div>
+        </>
+    ); 
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
-let time = new Date().toLocaleTimeString().toString();
-let hour = parseInt(time.split(":", 1));
-let median = time.split(" ")[1].toUpperCase();
-let greeting = '';
-let gtStyle = {};
-
-let h = new Date();
-console.log(Date.now());
-
-if(median === 'AM'){
-  greeting = "Good Morning";
-  gtStyle.color = "green";
-} 
-else if((hour === 12 || hour <= 7) && median === 'PM'){
-  greeting = "Good Afternoon";
-  gtStyle.color = "orange";
-}
-else{
-  greeting = "Good Night";
-  gtStyle.color = "black";
-}
+import App from './App.jsx';
 
 ReactDOM.render(
-  <>
-    <div className="main-div">
-      <h1>Hello Sir, <span style={ gtStyle }> { greeting } </span></h1>
-    </div>
-  </>,
+  <App />,
   document.getElementById('root')
 );
