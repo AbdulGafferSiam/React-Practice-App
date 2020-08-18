3b3e37290c8e1a93e4ccf7a3034a629f66193e31
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-18 14:24:52 +0900

    CSS Styling & Importing CSS Files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
 const name = "Siam";
 const imgURL1 = "https://picsum.photos/536/354";
@@ -7,16 +8,16 @@ const imgURL2 = "https://picsum.photos/536/355";
 const imgURL3 = "https://picsum.photos/536/356";
 const link = "http://www.siamsbucket.com/";
 
-// JSX Attributes
-// Use camel case of global attributes
 ReactDOM.render(
   <>
-    <h1 contentEditable="true">My name is { name } </h1>
-    <img src={imgURL1} alt="random images" />
-    <img src={imgURL2} alt="random images" />
-    <a href={link} target="_blank">
-      <img src={imgURL3} alt="random images" />
-    </a>
+    <h1 className="header">My name is { name } </h1>
+    <div className="img_div">
+      <img src={imgURL1} alt="random images" />
+      <img src={imgURL2} alt="random images" />
+      <a href={link} target="_blank">
+        <img src={imgURL3} alt="random images" />
+      </a>
+    </div>
   </>,
   document.getElementById('root')
 );
