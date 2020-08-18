f90775b1960eadc66a5b1f8b36364a700596e584
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-18 22:07:27 +0900

    Functional Component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from './Heading';
+import Paragraph from './Paragraph';
+import MovieList from './MovieList';
+
+
+function App() {
+    return (
+        <>
+            <Heading />
+            <Paragraph />
+            <MovieList />
+        </>
+    );
+}
+
+export default App;
