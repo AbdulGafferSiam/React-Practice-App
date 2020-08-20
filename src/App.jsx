c99e409dc0b5a9a4b85e71edb172ad9c526b261d
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-20 21:12:05 +0900

    Handling Events in ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,34 @@
 import React, { useState } from 'react';
 import './index.css';
 
-/* 
-1. Hooks: Start from react v16.8
-2. It allows you to use state and other react features without writing a class.
-   Hooks are the functions which 'hook into' react state and
-   lifecycle features from function components.
-3. It does not work inside clases.
-4. Hooks should always be used at the top level
-   of the react functions.
-*/
-
-/*
-useState() / hook : useState is a function which return an array of two item.
-First item is 'undefined' (state object / current data) and 2nd item is a function (updated function / updated data).
-console.log(useState());
-*/
-
 const App = () => {
-    let currTime = new Date().toLocaleTimeString();
-    // Array destructuring
-    let [time, setTime] = useState(currTime);
+    const purple = "#8e44ad";
+    const newColor = "#34495e";
+    
+    const btnDefaultText = "Click Me";
+    const btnUpdText = "Ayyo 😺"; 
 
-    const UpdTime = () => {
-        setTime(new Date().toLocaleTimeString());
-    };
+    const [currBg, setBg] = useState(purple);
 
-    setInterval(UpdTime, 1000);
+    const [currText, setText] = useState(btnDefaultText);
+
+    const bgChange = () => {
+        setBg(newColor);
+        setText(btnDefaultText);
+    }
+
+    const btnChange = () => {
+        setBg(purple);
+        setText(btnUpdText);
+    }
 
     return (
         <>
-            <div>
-                <h1> { time } </h1>
+            <div style={{backgroundColor: currBg}}>
+                <button onDoubleClick={btnChange} onClick={bgChange}> { currText } </button>
             </div>
         </>
     );
-};
+}
 
 export default App;
