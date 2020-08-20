9dffeab9dd5a03e82c01dbdc8665548e55d63503
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-20 16:47:45 +0900

    useState in Hook in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
-const SlotM = () => {
-    let x = ['😇', '😍', '😃'];
-    let y = ['😇', '😍', '😃'];
-    let z = ['😇', '😍', '😃'];
+/* 
+1. Hooks: Start from react v16.8
+2. It allows you to use state and other react features without writing a class.
+   Hooks are the functions which 'hook into' react state and
+   lifecycle features from function components.
+3. It does not work inside clases.
+4. Hooks should always be used at the top level
+   of the react functions.
+*/
 
-    let a = Math.floor(Math.random() * 3); 
-    let b = Math.floor(Math.random() * 3);
-    let c = Math.floor(Math.random() * 3);
+/*
+useState() / hook : useState is a function which return an array of two item.
+First item is 'undefined' (state object / current data) and 2nd item is a function (updated function / updated data).
+console.log(useState());
+*/
 
-    if ((a === b) && (a === c)) {
-        return (
-            <>
-                <div className="slot">
-                    <h1> {x[a]} {y[b]} {z[c]} </h1>
-                    <h1> This is matching </h1>
-                    <hr />
-                </div>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <div className="slot">
-                    <h1> {x[a]} {y[b]} {z[c]} </h1>
-                    <h1> This is not matching </h1>
-                    <hr />
-                </div>
-            </>
-        );
-    }
-}
-
-// :slot_machine:
 const App = () => {
+    // Array destructuring
+    let [count, setCount] = useState(0);
+
+    const IncNum = () => {
+        setCount(count + 1);
+    };
+
     return (
         <>
-            <h1 className="heading_style">
-                🎰 Welcome to {" "}
-                <span style={{ fontWeight: "bold", backgroundColor: "#fff" }}> Slot machine game </span> 🎰
-            </h1>
-            <SlotM />
-            <SlotM />
-            <SlotM />
-            <SlotM />
+            <div>
+                <h1> { count } </h1>
+                <button onClick={ IncNum }> Click me </button>
+            </div>
         </>
     );
-}
+};
 
 export default App;
