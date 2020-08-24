53cb3278e25cc9cf6b9a6a49838c11acbd5b8494
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-24 15:45:22 +0900

    use Material UI Icons in React App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
 import './index.css';
 
 const App = () => {
     const [val, setVal] = useState(0);
-    
+
     const btnLeft = () => {
         setVal(val + 1);
     }
@@ -25,8 +27,8 @@ const App = () => {
                 <div className="sub_div">
                     <h1> { val } </h1>
                     <br />
-                    <button onClick={btnLeft}> Increament </button>
-                    <button onClick={btnRight}> Decreament </button>
+                    <button onClick={btnLeft}> <AddIcon /> </button>
+                    <button onClick={btnRight}> <DeleteIcon /> </button>
                 </div>
             </div>
         </>
