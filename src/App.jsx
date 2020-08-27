167b89208968ae4cdc31cb3e786e6fa23b700660
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-27 16:54:51 +0900

    useEffect Hook in React JS-lifecycle model

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
-import React, { createContext } from 'react';
-import CompA from './CompA';
+import React, { useState, useEffect, useRef } from 'react';
+import ClassLifeCycle from './ClassLifeCycle';
 
-/*
-React Context Api:
-Allows you to easily access data at different levels
-of the component tree, without passing prop to every level
+// whenever call the render function, useEffect()
+// will be execute automatically
 
-step 1: createContext()
-step 2: provider
-step 3: consumer
-*/
+// the empty array means, the first time when load the page then 
+// only execute the useEffect()
+// useEffect() takes only a function inside it
 
-const FirstName = createContext();
-const LastName = createContext();
-
+// simulate componentDidMount(), componentDidUpdate(), componentDidUnmount()
 const App = () => {
+    const [num, setNum] = useState(0);
+    const [nums, setNums] = useState(0);
+
+    // The empty array means, the first time when load the page then 
+    // execute the useEffect()
+    /*
+        useEffect(() => {
+            alert("Button clicked");
+        }, []);
+    */
+
+    // If the value of "num" change, then useEffect() will execute
+    /*
+        useEffect(() => {
+            alert("Button clicked");
+        }, [num]); 
+    */
+    
+    // how do we prevent it to run on initial render for some cases?
+    // We can use useRef hook to solve this issue
+    // useRef() -> useRef hook can store any mutable value we want, so we could use that to keep track of if it’s the first time the useEffect function is being run
+
+    const firstUpdate = useRef(true);
+
+    useEffect(() => {
+        if(firstUpdate.current) {
+            firstUpdate.current = false;
+            return;
+        }
+        alert("Button clicked");
+    }, [num]);
 
     return (
         <>
-            <FirstName.Provider value={"Siam"}>
-                <LastName.Provider value={"Ahmed"}>
-                    <CompA />
-                </LastName.Provider>
-            </FirstName.Provider>
+            <button onClick={() => setNum(num + 1)}> Click {num} </button>
+            <br />
+            <button onClick={() => setNums(nums + 1)}> Click {nums} </button>
+            <ClassLifeCycle />
         </>
     );
 }
 
 export default App;
-export { FirstName, LastName };
