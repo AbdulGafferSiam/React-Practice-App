6a6fa689b59f2bfe0afc84588fb97571b6775095
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-30 13:57:08 +0900

    useHistory Hook in React Router

--- src/User.jsx
+++ src/User.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, useHistory } from 'react-router-dom';
 import './index.css';
 
 // previous approach withot using useParams()
@@ -14,21 +14,28 @@ const User = ({ match }) => {
 }
 */
 
-// using useParams()
 const User = () => {
+    // using useParams()
     // perameter name need to same as route name
     const { fname, lname } = useParams();
     // useLocation() use to get the current url
     const location = useLocation();
+    // useHistory() use to get track of the previous url
+    const history = useHistory();
 
     return (
         <>
             <h1 style={{ textTransform: "capitalize" }}>{fname} {lname} Page</h1>
             <p> My current location is {location.pathname} </p>
             {location.pathname === `/user/siam/ahmed` ?
-                <button onClick={() => {
-                    alert("Correct URL!");
-                }}> Click </button> : null}
+                <>
+                    <button onClick={() => {
+                        history.goBack();
+                    }}> Back </button>
+                    <button onClick={() => {
+                        history.push('/');
+                    }}> Home </button>
+                </> : null}
         </>
     );
 }
