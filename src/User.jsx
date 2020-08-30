b59e8f4801fb2ee421733684d259f8394844b7b3
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-08-30 13:43:49 +0900

    useLocation Hook in React Router

--- src/User.jsx
+++ src/User.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import './index.css';
 
 // previous approach withot using useParams()
@@ -18,9 +18,17 @@ const User = ({ match }) => {
 const User = () => {
     // perameter name need to same as route name
     const { fname, lname } = useParams();
+    // useLocation() use to get the current url
+    const location = useLocation();
+
     return (
         <>
-            <h1 style={{textTransform:"capitalize"}}>{fname} {lname} Page</h1>
+            <h1 style={{ textTransform: "capitalize" }}>{fname} {lname} Page</h1>
+            <p> My current location is {location.pathname} </p>
+            {location.pathname === `/user/siam/ahmed` ?
+                <button onClick={() => {
+                    alert("Correct URL!");
+                }}> Click </button> : null}
         </>
     );
 }
