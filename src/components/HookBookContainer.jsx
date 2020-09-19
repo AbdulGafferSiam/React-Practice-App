544c29c84c601e9b7130cdf6f37452477200b0f0
unknown <md.abdul.gaffer.siam@kanto.sysystem>
2020-09-19 22:45:33 +0900

    React Redux : Action Payload

--- src/components/HookBookContainer.jsx
+++ src/components/HookBookContainer.jsx
@@ -1,6 +1,7 @@
 // Install react extention
 // Type "rfc" to create component implicitly
 import React from 'react';
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { buyBook } from '../redux';
 import '../index.css';
@@ -9,12 +10,32 @@ const HookBookContainer = () => {
     const numOfBooks = useSelector(state => state.numOfBooks)
     const dispatch = useDispatch();
 
+    const [input, setInput] = useState(1);
+
+    const onChangeHandler = (event) => {
+        const val = event.target.value;
+        setInput(val);
+    }
+
     return (
         <>
             <div className="book-container">
                 <h1> Hook Container </h1>
-                <h3> Number of Books - { numOfBooks } </h3>
-                <button onClick={ () => dispatch(buyBook()) }> Buy Book </button>
+                <h3> Number of Books - { numOfBooks > 0 ? numOfBooks : 0 } </h3>
+                <input type="text" value={ input } onChange = { onChangeHandler } />
+                <button onClick={ () => {
+                    if (numOfBooks > 0 && numOfBooks >= input) {
+                        dispatch(buyBook(input));
+                        setInput(1);
+                    }
+                    
+                } }> Buy Book </button>
+                {
+                    numOfBooks < 1 ? <p style={{color:"red"}}> Book is empty </p> : null
+                }
+                {
+                    numOfBooks < input ? <p style={{color:"red"}}> Not enough book in store </p> : null
+                }
             </div>
         </>
     )
--- src/redux/book/BookReducer.jsx
+++ src/redux/book/BookReducer.jsx
@@ -8,7 +8,7 @@ const bookReducer = (state=initialState, action) => {
     switch(action.type) {
         case BUY_BOOK: return {
             ...state,
-            numOfBooks: state.numOfBooks - 1
+            numOfBooks: state.numOfBooks - action.payload
         }
         default: return state;
     }
